4a9cae60b2dd41323a668958ecc25383222b33b0
mthh <matthieu.viry@univ-grenoble-alpes.fr>
2021-03-01 10:09:30 +0100

    ...

--- _static/js/custom.js
+++ _static/js/custom.js
@@ -11,6 +11,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
   });
   var linkdoc = document.createElement('p');
   linkdoc.id ='link-doc-pyqgis';
-  linkdoc.innerHTML = '<a href="https://qgis.org/pyqgis/3.4/" target="_blank">Documentation de l\'API <b>PyQgis</b><img src="_static/magnify-clip-ltr.svg" /></a>';
+  linkdoc.innerHTML = '<a href="https://qgis.org/pyqgis/3.16/" target="_blank">Documentation de l\'API <b>PyQgis</b><img src="_static/magnify-clip-ltr.svg" /></a>';
   document.querySelector('.sphinxsidebar').appendChild(linkdoc);
 });
